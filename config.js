33a621e3fe0928df12704f491faa53567fcdc8b0
HertzG <georgedot@gmail.com>
2012-12-23 02:20:49 +0400

    Update config.js

--- config.js
+++ config.js
@@ -26,11 +26,11 @@ exports.hosts = {
 //    'www.example.com': 'http://example.com/',
 //    'my.example.com': 'http://example.com/my',
 //	  'your.example.com': {
-//	        code: 301,
+//          code: 301,
 //          host: 'http://example.com/your'
 //    },
 //	  'his.example.com': {
-//	        code: 307,  //Temporary Redirect
+//          code: 307,  //Temporary Redirect
 //          host: 'http://example.com/your'
 //    },
 // };
