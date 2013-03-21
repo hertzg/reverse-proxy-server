f8c1f16f547d2f93b205c6b37a8549e2b28d1e86
archil <archil@archil-laptop.(none)>
2013-03-22 02:36:52 +0400

    divide master and workers

---
+++ master.js
@@ -0,0 +1,60 @@
+var cluster = require('cluster'),
+    fs = require('fs'),
+    os = require('os')
+
+var verbose = process.argv.indexOf('-v') != -1
+
+var config = (function () {
+    var homeDir = process.env.HOME
+    var configFile = homeDir + '/.reverse-proxy-server/config.js'
+    if (!fs.existsSync(configFile)) {
+        configFile = './config.js'
+    }
+    if (verbose) {
+        console.log('Reading config file from "' + configFile + '".')
+    }
+    return require(configFile)
+})()
+
+var hosts = Object.create(null)
+for (var i in config.hosts) {
+    hosts[i] = config.hosts[i]
+}
+
+var redirectHosts = Object.create(null)
+for (var i in config.redirectHosts) {
+    var redirectHost = config.redirectHosts[i]
+    if (typeof redirectHost == 'string') {
+        redirectHost = { code: 301, host: redirectHost }
+    }
+    redirectHosts[i] = redirectHost
+}
+
+var removeHeaders = config.removeHeaders
+
+var replaceErrorPages = Object.create(null)
+for (var i in config.replaceErrorPages) {
+    var filename = config.replaceErrorPages[i]
+    try {
+        replaceErrorPages[i] = fs.readFileSync(filename, 'utf8')
+    } catch (e) {
+        console.error('Failed to read error page "' + filename + '"')
+    }
+}
+
+var initMessage = {
+    host: config.host,
+    port: config.port,
+    hosts: hosts,
+    redirectHosts: redirectHosts,
+    removeHeaders: removeHeaders,
+    replaceErrorPages: replaceErrorPages,
+}
+
+var numCpus = os.cpus().length
+for (var i = 0; i < numCpus; i++) {
+    var worker = cluster.fork()
+    worker.on('online', function () {
+        worker.send(initMessage)
+    })
+}
---
+++ worker.js
@@ -0,0 +1,130 @@
+function htmlspecialchars (s) {
+    s = s.replace(/&/g, '&amp;')
+    s = s.replace(/</g, '&lt;')
+    s = s.replace(/>/g, '&gt;')
+    s = s.replace(/"/g, '&quot;')
+    s = s.replace(/'/g, '&#39;')
+    return s
+}
+
+var http = require('http')
+
+process.on('message', function (initMessage) {
+
+    var port = initMessage.port,
+        host = initMessage.host,
+        hosts = initMessage.hosts,
+        redirectHosts = initMessage.redirectHosts,
+        removeHeaders = initMessage.removeHeaders,
+        replaceErrorPages = initMessage.replaceErrorPages
+
+    http.createServer(function (req, res) {
+
+        function proxify (req, res, host) {
+
+            function sendBadGateway () {
+                sendErrorPage(502)
+            }
+
+            if (req.httpVersion == '1.0') {
+                req.headers.connection = 'close'
+            } else {
+                req.headers.connection = 'keep-alive'
+            }
+
+            var requestConfig = {
+                host: host.host,
+                port: host.port,
+                method: req.method,
+                path: req.url,
+                headers: req.headers,
+            }
+
+            var proxyReq = http.request(requestConfig, function (proxyRes) {
+
+                proxyReq.removeListener('error', sendBadGateway)
+                proxyReq.on('error', function () {
+                    res.end()
+                })
+
+                for (var i in removeHeaders) {
+                    delete proxyRes.headers[removeHeaders[i]]
+                }
+
+                var statusCode = proxyRes.statusCode
+                var replaceErrorPage = replaceErrorPages[statusCode]
+                if (replaceErrorPage) {
+                    res.statusCode = statusCode
+                    res.end(replaceErrorPage)
+                    proxyReq.abort()
+                } else {
+                    res.writeHead(statusCode, proxyRes.headers)
+                    proxyRes.on('data', function (data) {
+                        res.write(data)
+                    })
+                    proxyRes.on('end', function () {
+                        res.end()
+                    })
+                }
+
+            })
+
+            proxyReq.on('error', sendBadGateway)
+            req.on('data', function (data) {
+                proxyReq.write(data)
+            })
+            req.on('end', function () {
+                proxyReq.end()
+            })
+            res.on('error', function () {
+                proxyReq.removeListener('error', sendBadGateway)
+                proxyReq.abort()
+            })
+
+        }
+
+        function sendErrorPage (statusCode) {
+            var title = statusCode + ' ' + http.STATUS_CODES[statusCode]
+            res.writeHead(statusCode, {
+                'Content-Type': 'text/html; charset=UTF-8',
+            })
+            sendPage({
+                head: '<title>' + title + '</title>',
+                body: '<h1>' + title + '</h1>'
+            })
+        }
+
+        function sendPage (e) {
+            res.end(
+                '<!DOCTYPE html>' +
+                '<html>' +
+                    '<head>' + e.head + '</head>' +
+                    '<body>' + e.body + '</body>' +
+                '</html>'
+            )
+        }
+
+        var hostHeader = req.headers['host'],
+            redirectHost = redirectHosts[hostHeader]
+        if (redirectHost) {
+            var statusCode = redirectHost.code,
+                title = statusCode + ' ' + http.STATUS_CODES[statusCode],
+                location = redirectHost.host + req.url
+            res.writeHead(statusCode, {
+                'Content-Type': 'text/html; charset=UTF-8',
+                Location: location,
+            })
+            sendPage({
+                head: '<title>' + title + '</title>',
+                body: '<h1>' + title + '</h1>' +
+                    '<p>The document has moved <a href="' + htmlspecialchars(location) + '">here</a>.</p>'
+            })
+        } else {
+            var host = hosts[hostHeader]
+            if (host) proxify(req, res, host)
+            else sendErrorPage(404)
+        }
+
+    }).listen(port, host)
+
+})
